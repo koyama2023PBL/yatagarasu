8c6b5f831d449d6bd87e38282103830d3e829490
a2244ky <a2244ky@aiit.ac.jp>
2023-10-29 17:27:53 +0900

    feat: キャッシュヒット率の取得を実装

--- src/Content/Postgresql/CacheHitRate.tsx
+++ src/Content/Postgresql/CacheHitRate.tsx
@@ -1,33 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import instance from '../../Axios/AxiosInstance';
-import { roundToTwoDecimalPlaces, unixTimeToDate, calcAverage} from '../../Component/Common/Util';
-import { Card, CardContent, Typography, IconButton, Popover, Box, CircularProgress } from '@mui/material';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import ErrorIcon from '@mui/icons-material/Error';
-import WarningIcon from '@mui/icons-material/Warning';
-import { green, yellow, red } from '@mui/material/colors';
-import { prometheusSettings } from '../../Component/Redux/PrometheusSettings';
-
-// interface CacheHitRateData {
-//   startTime: string;
-//   endTime: string;
-//   databaseName: string;
-//   hitRate: number;
-// }
-
-// interface CacheHitRateApiResponse {
-//   starttime: string;
-//   endtime: string;
-//   dbname: string;
-//   hitrate: number;
-// }
-
-// interface CacheHitRateApiRequest {
-//   starttime: Date;
-//   endtime: Date;
-//   dbname: string;
-// }
+import React, { useEffect, useState } from "react";
+import instance from "../../Axios/AxiosInstance";
+import {
+  roundToTwoDecimalPlaces,
+  unixTimeToDate,
+  calcAverage,
+} from "../../Component/Common/Util";
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+  Popover,
+  Box,
+  CircularProgress,
+} from "@mui/material";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ErrorIcon from "@mui/icons-material/Error";
+import WarningIcon from "@mui/icons-material/Warning";
+import { green, yellow, red } from "@mui/material/colors";
+import { prometheusSettings } from "../../Component/Redux/PrometheusSettings";
+import yatagarasuSettings from "../../Component/Redux/YatagarasuSettings";
 
 interface CacheHitRateProps {
   starttime: Date;
@@ -35,22 +28,22 @@ interface CacheHitRateProps {
 }
 
 //APIレスポンスの基本形
-export interface CacheHitRateApiResponse{
+export interface CacheHitRateApiResponse {
   data: CacheHitRateResponseData;
   status: string;
 }
 
-export interface CacheHitRateResponseData{
+export interface CacheHitRateResponseData {
   resultType: string;
   result: CacheHitRateResponseResult[];
 }
 
-export interface CacheHitRateResponseResult{
+export interface CacheHitRateResponseResult {
   metric: CacheHitRateResponseMetric;
   values: [number, string][];
 }
 
-export interface CacheHitRateResponseMetric{
+export interface CacheHitRateResponseMetric {
   __name__: string;
   datid: string;
   datname: string;
@@ -58,64 +51,80 @@ export interface CacheHitRateResponseMetric{
   job: string;
 }
 
-export interface CacheHitRateApiRequest{
+export interface CacheHitRateApiRequest {
   //query: string;
   start: Date;
   end: Date;
   datname: string;
 }
 
-
-
-const fetchFromAPIwithRequest = async (endpoint: string, queryParameters: CacheHitRateApiRequest, query: string) => {
+const fetchFromAPIwithRequest = async (
+  endpoint: string,
+  queryParameters: CacheHitRateApiRequest,
+  query: string
+) => {
   try {
-      const startTimeString = queryParameters.start.toISOString().toString();
-      const endTimeString = queryParameters.end.toISOString().toString();
+    const startTimeString = queryParameters.start.toISOString().toString();
+    const endTimeString = queryParameters.end.toISOString().toString();
 
-      const response = await instance.get<CacheHitRateApiResponse>(
-        `${endpoint}${encodeURIComponent(query)}&start=${startTimeString}&end=${endTimeString}&step=${prometheusSettings?.postgresqlScrapeInterval}`
-      );
-      return { status: response.status, data: response.data };
+    const response = await instance.get<CacheHitRateApiResponse>(
+      `${endpoint}${encodeURIComponent(
+        query
+      )}&start=${startTimeString}&end=${endTimeString}&step=${
+        prometheusSettings?.postgresqlScrapeInterval
+      }`
+    );
+    return { status: response.status, data: response.data };
   } catch (err) {
-      console.log("err:", err);
-      throw err;
+    console.log("err:", err);
+    throw err;
   }
-}
+};
 
 const CacheHitRate: React.FC<CacheHitRateProps> = ({ starttime, endtime }) => {
-  const [cacheHitRateData, setCacheHitRateData] = useState<CacheHitRateResponseData | null>(null);
+  const [cacheHitRateData, setCacheHitRateData] =
+    useState<CacheHitRateResponseData | null>(null);
   const [statusCode, setStatusCode] = useState<number | null>(null);
-  const [datname, setDatname] = useState<string>('explorer');
+  const [datname, setDatname] = useState<string | null>(null);
 
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLButtonElement>) => {setAnchorEl(event.currentTarget);};
-  const handlePopoverClose = () => {setAnchorEl(null);};
+  const handlePopoverOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
   const open = Boolean(anchorEl);
 
   //const [cacheHitAverage, setCacheHitAverage] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchCacheHitRateData = async () => {
-      const endpoint = '/api/v1/query_range?query='
-      const query = 'pg_stat_database_blks_hit{datname="explorer"}/(pg_stat_database_blks_hit{datname="explorer"}+pg_stat_database_blks_read{datname="explorer"})';
+      const endpoint = "/api/v1/query_range?query=";
+      const query =
+        'pg_stat_database_blks_hit{datname="' +
+        yatagarasuSettings.dbname +
+        '"}/(pg_stat_database_blks_hit{datname="' +
+        yatagarasuSettings.dbname +
+        '"}+pg_stat_database_blks_read{datname="' +
+        yatagarasuSettings.dbname +
+        '"})';
 
       const requestBody: CacheHitRateApiRequest = {
         start: new Date(starttime),
         end: new Date(endtime),
-        datname: datname,
+        datname: yatagarasuSettings.dbname,
       };
 
-      const { status, data: response }: {status: number, data: CacheHitRateApiResponse}  = await fetchFromAPIwithRequest(
-        endpoint,
-        requestBody,
-        query
-      );
+      const {
+        status,
+        data: response,
+      }: { status: number; data: CacheHitRateApiResponse } =
+        await fetchFromAPIwithRequest(endpoint, requestBody, query);
       setStatusCode(status);
       setCacheHitRateData(response.data);
-      // if (response.data.result[0].values.length !== 0){
-      //   setCacheHitAverage(calcAverage(response.data.result[0].values));
-      // }
-      
+      //外部定義ファイルからの設定
+      setDatname(yatagarasuSettings.dbname);
     };
 
     fetchCacheHitRateData();
@@ -123,7 +132,7 @@ const CacheHitRate: React.FC<CacheHitRateProps> = ({ starttime, endtime }) => {
 
   const getIcon = () => {
     if (statusCode === null) {
-      return WarningIcon; 
+      return WarningIcon;
     }
 
     if (statusCode >= 500) {
@@ -133,13 +142,13 @@ const CacheHitRate: React.FC<CacheHitRateProps> = ({ starttime, endtime }) => {
     } else {
       return CheckCircleOutlineIcon;
     }
-  }
+  };
 
   const getIconColor = () => {
     if (statusCode === null) {
-      return; 
+      return;
     }
-  
+
     if (statusCode >= 500) {
       return red[500];
     } else if (statusCode >= 400) {
@@ -147,19 +156,32 @@ const CacheHitRate: React.FC<CacheHitRateProps> = ({ starttime, endtime }) => {
     } else {
       return green[500];
     }
-
-
-  }
+  };
 
   return (
-    <Card sx={{ width: '100%' }}>
+    <Card sx={{ width: "100%" }}>
       <CardContent>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' , marginTop: '-5px'}}>
-          <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            width: "100%",
+            marginTop: "-5px",
+          }}
+        >
+          <Typography variant="body2" sx={{ fontWeight: "bold" }}>
             Cache Hit Rate (%)
           </Typography>
-          {cacheHitRateData && React.createElement(getIcon(), { style: { color: getIconColor() } })}
-          <IconButton onClick={handlePopoverOpen} size="small" style={{ marginLeft: '-3px', marginRight: '-1px' }}>
+          {cacheHitRateData &&
+            React.createElement(getIcon(), {
+              style: { color: getIconColor() },
+            })}
+          <IconButton
+            onClick={handlePopoverOpen}
+            size="small"
+            style={{ marginLeft: "-3px", marginRight: "-1px" }}
+          >
             <HelpOutlineIcon fontSize="small" />
           </IconButton>
           <Popover
@@ -167,16 +189,26 @@ const CacheHitRate: React.FC<CacheHitRateProps> = ({ starttime, endtime }) => {
             anchorEl={anchorEl}
             onClose={handlePopoverClose}
             anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
+              vertical: "top",
+              horizontal: "left",
             }}
             transformOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
+              vertical: "bottom",
+              horizontal: "left",
             }}
           >
-            <Typography sx={{ p: 2 , alignItems: 'center'}} style={{ width: '600px', whiteSpace: 'pre-line' }}>
-              <HelpOutlineIcon fontSize="small" sx={{ marginBottom: '-5px', marginLeft: '3px', marginRight: '3px'}}/>
+            <Typography
+              sx={{ p: 2, alignItems: "center" }}
+              style={{ width: "600px", whiteSpace: "pre-line" }}
+            >
+              <HelpOutlineIcon
+                fontSize="small"
+                sx={{
+                  marginBottom: "-5px",
+                  marginLeft: "3px",
+                  marginRight: "3px",
+                }}
+              />
               pg_stat_databaseから取得したキャッシュヒット率です。キャッシュヒット率が低いとクエリの実行にかかる時間が長くなり、パフォーマンスの低下に繋がります。この値を下げる要因には以下のようなものがあります。
               <ul>
                 <li>メモリ不足</li>
@@ -187,44 +219,128 @@ const CacheHitRate: React.FC<CacheHitRateProps> = ({ starttime, endtime }) => {
             </Typography>
           </Popover>
         </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'left', width: '100%', height: '3.5vh'}}></Box>
-        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'left', width: '100%', marginLeft: '1vw'}}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "left",
+            width: "100%",
+            height: "3.5vh",
+          }}
+        ></Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "left",
+            width: "100%",
+            marginLeft: "1vw",
+          }}
+        >
           {cacheHitRateData ? (
             <>
-              <Card sx={{ width: '30%', marginRight: '1vw'}}>
+              <Card sx={{ width: "30%", marginRight: "1vw" }}>
                 <CardContent>
-                  <Typography variant="body2" align="left" sx={{ fontWeight: 'bold' , marginTop: '-1vh'}}>
+                  <Typography
+                    variant="body2"
+                    align="left"
+                    sx={{ fontWeight: "bold", marginTop: "-1vh" }}
+                  >
                     Database Name
                   </Typography>
-                  <Box sx={{justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: '1.5vh' }}>
-                    <Typography variant="h5" component="div" align="center" sx={{ display: 'inline' }}>
+                  <Box
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      width: "100%",
+                      marginTop: "1.5vh",
+                    }}
+                  >
+                    <Typography
+                      variant="h5"
+                      component="div"
+                      align="center"
+                      sx={{ display: "inline" }}
+                    >
                       {datname}
                     </Typography>
                   </Box>
                 </CardContent>
               </Card>
-              <Card sx={{ width: '60%'}}>
+              <Card sx={{ width: "60%" }}>
                 <CardContent>
-                  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' , marginTop: '-1vh'}}>
-                    <Typography variant="body2" align="left" sx={{ fontWeight: 'bold' }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      height: "100%",
+                      marginTop: "-1vh",
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      align="left"
+                      sx={{ fontWeight: "bold" }}
+                    >
                       Cache Hit Rate
                     </Typography>
-                    <Box sx={{width: '100%' ,height: '0.5vh'}}></Box>
-                    <Box sx={{justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: '1vh' }}>
-                      <Typography variant="h5" component="div" align="center" sx={{ display: 'inline' }}>
-                        {cacheHitRateData.result[0].values.length === 0 ? "-" : `${roundToTwoDecimalPlaces(calcAverage(cacheHitRateData.result[0].values) * 100)}%`}
+                    <Box sx={{ width: "100%", height: "0.5vh" }}></Box>
+                    <Box
+                      sx={{
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "100%",
+                        marginTop: "1vh",
+                      }}
+                    >
+                      <Typography
+                        variant="h5"
+                        component="div"
+                        align="center"
+                        sx={{ display: "inline" }}
+                      >
+                        {cacheHitRateData.result[0].values.length === 0
+                          ? "-"
+                          : `${roundToTwoDecimalPlaces(
+                              calcAverage(cacheHitRateData.result[0].values) *
+                                100
+                            )}%`}
                       </Typography>
                     </Box>
-                    <Typography variant="body2" component="div" align="right" sx={{ marginTop: '1vh', marginBottom: '-1.3vh'}}>
-                        Time: {unixTimeToDate(cacheHitRateData.result[0].values[1][0]).toLocaleString()} - {unixTimeToDate(cacheHitRateData.result[0].values[cacheHitRateData.result[0].values.length - 1][0]).toLocaleString()}
+                    <Typography
+                      variant="body2"
+                      component="div"
+                      align="right"
+                      sx={{ marginTop: "1vh", marginBottom: "-1.3vh" }}
+                    >
+                      Time:{" "}
+                      {unixTimeToDate(
+                        cacheHitRateData.result[0].values[1][0]
+                      ).toLocaleString()}{" "}
+                      -{" "}
+                      {unixTimeToDate(
+                        cacheHitRateData.result[0].values[
+                          cacheHitRateData.result[0].values.length - 1
+                        ][0]
+                      ).toLocaleString()}
                     </Typography>
                   </Box>
                 </CardContent>
               </Card>
             </>
           ) : (
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'left', width: '100%'}}>
-              <CircularProgress sx={{marginTop: '3.5vh', marginLeft: '-2vw'}}/>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "left",
+                width: "100%",
+              }}
+            >
+              <CircularProgress
+                sx={{ marginTop: "3.5vh", marginLeft: "-2vw" }}
+              />
             </Box>
           )}
         </Box>
