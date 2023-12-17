434768377ce2c2bb89f701872da00856060fd47b
a2244ky <a2244ky@aiit.ac.jp>
2023-12-17 13:04:42 +0900

    temp: 1217時点の仮ファイル

--- src/settingContext.tsx
+++ src/settingContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import yaml from 'js-yaml';
 
 const SettingsContext = createContext<Settings | undefined>(undefined);
@@ -21,18 +21,47 @@ export const useSettings = () => {
 }
 
 type Props = {
-    children?: React.ReactNode;
+    children?: ReactNode;
 };
 
-export const SettingsProvider = React.FC = ({ children }) => {
+export const SettingsProvider =  ({children}: Props) => {
     const [settings, setSettings] = useState<Settings>();
 
     useEffect(() => {
-        fetch('/settings.yml')
-            .then(response => response.text())
-            .then(data => yaml.load(data) as Settings)
-            .then(loadedSettings => setSettings(loadedSettings));
-    }, []);
+
+        //const fetchSettings = async () => {
+
+            fetch('/settings.yml')
+                .then(response => response.text())
+                .then(data => yaml.load(data) as Settings)
+                .then(loadedSettings => {
+                    if (typeof loadedSettings === 'object' && loadedSettings !== null){
+                        setSettings(loadedSettings);
+                    }
+                })
+                .catch(err => {
+                    console.log("err:", err);
+                });
+
+                
+
+            // const newSettings: Settings = {
+            //     baseURL: 'http://www.example.com',
+            //     dbname: 'exampleDB',
+            //     nodeExporterJobName: 'node_exporter',
+            //     postgresExporterJobName: 'postgres_exporter',
+            //     nodeExporterDefaultInterval: '15s',
+            //     postgresExporterDefaultInterval: '15s',
+            // };
+            // setSettings(newSettings);
+        }
+
+        // fetch('/settings.yml')
+        //     .then(response => response.text())
+        //     .then(data => yaml.load(data) as Settings)
+        //     .then(loadedSettings => setSettings(loadedSettings));
+        //fetchSettings();
+    );
     return (
         <SettingsContext.Provider value={settings}>
             {children}
