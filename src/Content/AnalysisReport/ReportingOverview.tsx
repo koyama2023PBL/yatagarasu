b79fdc54d60112d27cff08c114734f10095b1bb0
horikiri <a2238rh@aiit.ac.jp>
2024-01-02 09:56:25 +0900

    fix: レイアウト修正

--- src/Content/AnalysisReport/ReportingOverview.tsx
+++ src/Content/AnalysisReport/ReportingOverview.tsx
@@ -8,21 +8,22 @@ import {getPerformanceStatus} from "./PerformanceReport";
 import {StatusType} from "./AnalysisReportUtil";
 import {getDiskStatus} from "./DiskReport";
 
+
 /**
  * ステータスに応じたアイコンを返す
  * @param status
  */
 export const getStatusIcon = (status: StatusType | null): React.JSX.Element => {
   if (status === null) {
-    return <CircularProgress style={{ width: "80%", height: "80%" }} sx={{ marginTop: '3vh' }}/>;
+    return <CircularProgress style={{ marginTop: '3vh' }}/>;
   }
   switch (status) {
     case 'OK':
-      return <CheckCircleIcon style={{ width: "50%", height: "50%" }} sx={{ color: green[500], height: '10vh' }} />;
+      return <CheckCircleIcon style={{ width: "50%", height: "50%" }} sx={{ color: green[500] }} />;
     case 'WARNING':
-      return <WarningIcon style={{ width: "50%", height: "50%" }} sx={{ color: yellow[700], height: '10vh'}} />;
+      return <WarningIcon style={{ width: "50%", height: "50%" }} sx={{ color: yellow[700] }} />;
     case 'ERROR':
-      return <ErrorIcon style={{ width: "50%", height: "50%" }} sx={{ color: red[500], height: '10vh'}} />;
+      return <ErrorIcon style={{ width: "50%", height: "50%" }} sx={{ color: red[500] }} />;
   }
 };
 
@@ -39,7 +40,7 @@ export const ReportingOverview: React.FC = () => {
           </Typography>
         </Box>
         <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', p: '1.5', marginTop: '1vh', marginLeft: '2vw' }}>
-          <Card sx={{width: '15vw'}}>
+          <Card sx={{width: '15vw', height: '20vh'}}>
             <CardContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
               <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Typography variant="h6" sx={{marginRight: '0.3vw'}}>
@@ -49,7 +50,7 @@ export const ReportingOverview: React.FC = () => {
               </Box>
             </CardContent>
           </Card>
-          <Card sx={{width: '15vw', marginLeft: '2vw' }}>
+          <Card sx={{width: '15vw', height: '20vh', marginLeft: '2vw' }}>
             <CardContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
               <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <Typography variant="h6" sx={{marginRight: '0.3vw'}}>
