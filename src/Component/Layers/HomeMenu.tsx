24f14a06bf3ba671914b6d0e4ca3f04c25aa5526
a2238rh <a2238rh@aiit.ac.jp>
2024-02-11 00:03:46 +0900

    fix: adjust home image's width to full HD

--- src/Component/Layers/HomeMenu.tsx
+++ src/Component/Layers/HomeMenu.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
 
 const HomeMenu: React.FC = () => {
   return (
     <div>
-      <Box sx={{ width: '100vw', marginLeft: 'auto', marginRight: 'auto', marginTop: '-1.2vh' }}>
+      <Box sx={{ width: '95.8vw', marginLeft: 'auto', marginRight: 'auto', marginTop: '-1.2vh' }}>
         <img
           src="./images/Home.jpg"
           style={{
